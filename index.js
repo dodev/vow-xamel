4d1e412d4388938e4b0ebac1f3170a8fa027287d
Petar Dodev <dodev@yandex-team.ru>
2014-09-08 14:27:05 +0400

    promisify parse

---
+++ index.js
@@ -0,0 +1,27 @@
+var vow = require('vow');
+var xamel = require('xamel');
+
+/**
+ *
+ * @param {String} xmlString
+ * @param {Object} options
+ * @returns {vow.Promise}
+ */
+function promisifiedParse(xmlString, options) {
+    var deferred = new vow.Deferred();
+
+    xamel.parse(xmlString, options, function (err, nodeSet) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            deferred.resolve(nodeSet);
+        }
+    });
+
+    return deferred.promise();
+}
+
+module.exports = {
+    parse: promisifiedParse,
+    serialize: xamel.serialize
+}
